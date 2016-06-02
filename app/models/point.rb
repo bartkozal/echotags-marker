f073182f6e7c3b8b0b44156198d5103101867fbf
Bartlomiej Kozal <bkzl@me.com>
2016-06-02 17:29:55 +0200

    Add categories to points json

--- app/models/point.rb
+++ app/models/point.rb
@@ -22,7 +22,7 @@ class Point < ActiveRecord::Base
   end
 
   def as_json(*)
-    json = {title: title, latitude: latitude, longitude: longitude, audio: audio}
+    json = {title: title, latitude: latitude, longitude: longitude, audio: audio, categories: categories}
     json[:triggers] = if triggers.present?
       triggers
     else
